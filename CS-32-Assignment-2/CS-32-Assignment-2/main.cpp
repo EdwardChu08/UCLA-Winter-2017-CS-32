bd116612a734b76477b803f2880f3c70495617f8
Edward Chu <Edwardchu08@gmail.com>
2017-01-30 14:58:43 -0800

    added report.docx

--- CS-32-Assignment-2/CS-32-Assignment-2/main.cpp
+++ CS-32-Assignment-2/CS-32-Assignment-2/main.cpp
@@ -214,7 +214,95 @@ int main() {
     
     
     cerr << endl << "All tests succeeded" << endl;
+    
+    /*
+    Sequence a;
+    
+    a.insert(0, 0); //Testing normal
+    a.insert(1, 1);
+    a.insert(2, 2);
+    a.insert(3, 3);
+    
+    ItemType test;
+    
+    assert(a.get(2, test)); //Testing get
+    assert(!a.get(4, test)); //Out of bounds
+    assert(test == 2);
+    
+    assert(a.erase(2)); //Test erase
+    //a.dump();
+    assert(a.get(2, test)); //Make sure erase works
+    assert(test == 3);
+    
+    Sequence b;
+    
+    b.insert(5);
+    b.insert(6);
+    b.insert(7);
+    b.insert(8);
+    //b.dump();
+    
+    a.swap(b); //Test swap
+    
+    a.dump();
+    b.dump();
+    
+    assert(a.get(1, test));
+    assert(!b.get(3, test));
+    assert(test == 6);
+    
+    //b.insert(1.5);
+    //assert(b.remove(1.5) == 2);
+    
+    //b.dump();
+    
+    b.insert(2, 1);
+    
+    unsigned long temp;
+    
+    assert(b.get(2, temp));
+    assert(temp == 1);
+    
+    
+    
+    assert(b.find(1) == 1); //Test find
+    assert(b.find(10) == -1); //Finding non-existent item
+    
+    assert(b.erase(1)); //Test erase
+    assert(b.get(1, test) && b.get(2, temp));
+    assert(test == 1 && temp == 3);
+    
+    b.set(2, 1); //Testing set
+    assert(b.get(2, test));
+    assert(test == 1);
+    
+    b.insert(4);
+    assert(b.find(4) == 3); //Testing find, multiple instance of same item
+    
+    b.insert(0); //Testing insert at start
+    assert(b.get(0, test) && b.get(1, temp));
+    assert(test == 0 && temp == 0);
+    
+    
+    for(int i = 3; i < 6; i++)
+        b.erase(3);
+    
+    
+    a.swap(b); //Testing swap
+    
+    assert(!a.get(4, temp));
+    assert(a.get(2, temp));
+    assert(temp == 1);
+    
+    //Bad
+    assert(!a.insert(10, 2)); //Out of bounds
+    assert(!a.insert(-1, 2)); //Out of bounds
+    
+    Sequence c(a); //Copy constructor
+    a.dump();
+    c.dump();
     return 0;
+     */
 }
 
 
